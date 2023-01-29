c9f8a27cb5e6c47114f66290b4790bd28cc6c5cd
unknown <0786malkeetsingh@gmail.com>
2023-01-29 11:42:16 +0530

    first commit

---
+++ minorProject-guardin/client/src/components/aboutpagecontent.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+import Zoom from 'react-reveal/Zoom';
+
+
+
+class AboutPageContent extends Component {
+  render() {
+    return (
+
+        <Zoom delay={400}>
+ <div className="about-area">
+        <div className="container p-0">
+            <div className="row m-0">
+                
+    
+                <div className="col-lg-12 col-md-12 p-0">
+                   
+                <div className="about">
+        <div className="container">
+            <div className="row">
+                
+
+                <div className="col-md-12">
+                  
+                    <h2>ABOUT US</h2>
+                    <div className="about-content">
+                        A people can focus on their work and their time by Using P2P delivery and can book delivery guy </div>
+
+                    </div>
+         
+               
+
+                
+            </div>
+        </div>
+    </div>
+
+
+   
+    <div className="col-md-12">
+
+    <br/><br/>
+
+    <div className="im">
+
+   <Zoom delay={500}>
+   <h2>Why Choose Us</h2>
+                        <br/><br/>
+                        <ul className="im-list">
+                                <li className="font-li">This is a online platform where you can easily book a deivery boy</li>
+								<br/>
+                                <li className="font-li"> We are committed to high quality P2PDelivery app across  across the country.</li>
+								
+								<br/>
+                               <li className="font-li"> It gives a sense of assurance so that the person can deliver their product freely without any worries</li>
+							   <br/>
+							     <li className="font-li">Just like a person asks a family member or a neighbor to keep an eye or live in the property while he/she is gone.</li>
+								 <br/>
+						<li className="font-li">After successful payment, custodian visits. Familiarise customer with list of documents required beforehand on platform.</li>
+						<br/>
+                        <li className="font-li">We understand the industry research needs thoroughly and before taking up orders having any Technical/regulatory concerns with respect to Process, Quality,Potency,Safet etc., from clients, our technical team is ready to interact and discuss with the technical team at client’s end to better understand the costumer concern/requirement for best outcome and timely delivery of quality products.  </li>
+							<br/>
+							<li className="font-li">	Paper work, proof of ownership, insurance, accessible and inaccessible quarters, final payment is collected by the custodian which is based on size of house, no. of floors and no. of pets/children/elders.If anyone want to be a custodian there are many types of custodian Type of custodian-Guard ,Maintainer,Caretaker</li> 
+							<br/><br/>
+						<li className="font-li">.Just fill in our short form, tell us your security requirements and call P2P Delivery to  make Work easier <br/>
+							<br/>It’s that simple!</li>
+							
+							
+							
+                            </ul></Zoom> 
+    </div>
+                       
+
+    </div>
+
+
+                      
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+        </Zoom>
+    );   
+  }
+}
+
+export default AboutPageContent;
